30dcdf766b89f99cebf3fc02aeeac102a9cab34b
De1c <denpro8350@gmail.com>
2023-03-20 17:51:15 +0100

    Version 1.0

--- homework/src/database/connect.py
+++ homework/src/database/connect.py
@@ -1,8 +1,15 @@
+import configparser
+import pathlib
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:567234@localhost:5432/postgres"
+file_config = pathlib.Path(__file__).parent.joinpath('config.ini')
+config = configparser.ConfigParser()
+config.read(file_config)
+
+SQLALCHEMY_DATABASE_URL = config.get('DB', 'url')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
